3547163b2a49dd3addb5c21a92f9d768cdeb3a8c
Ioannis G. Tsoulos <itsoulos@gmail.com>
2024-07-31 13:39:06 +0300

    fix ego

--- METHODS/ego.cpp
+++ METHODS/ego.cpp
@@ -3,7 +3,7 @@
 
 
 EGO::EGO() {
-    addParam(Parameter("ego_count","200","Number of  SearchAgents"));
+    addParam(Parameter("ego_count","50","Number of  SearchAgents"));
     addParam(Parameter("ego_maxiters","200","Maximum number of generations"));
     addParam(Parameter("ego_lrate","0.05","Localsearch rate"));
     addParam(Parameter("ego_iters","30","Number of iters"));
@@ -11,29 +11,32 @@ EGO::EGO() {
 
 }
 
-void EGO::init()
-{
-    SearchAgents=getParam("ego_count").getValue().toInt();
-    maxGenerations=getParam("ego_maxiters").getValue().toInt();
-    localsearchRate=getParam("ego_lrate").getValue().toDouble();
-    terminationMethod=getParam("gen_termination").getValue();
-    generation=0;
-    iters=getParam("ego_iters").getValue().toInt();
+void EGO::init() {
+    SearchAgents = getParam("ego_count").getValue().toInt();
+    maxGenerations = getParam("ego_maxiters").getValue().toInt();
+    localsearchRate = getParam("ego_lrate").getValue().toDouble();
+    terminationMethod = getParam("gen_termination").getValue();
+    generation = 0;
+    iters = getParam("ego_iters").getValue().toInt();
+
     Theseis.resize(SearchAgents);
     fitness.resize(SearchAgents);
-    D= myProblem->getDimension();
-    for(int i=0;i<SearchAgents;i++)
+    D = myProblem->getDimension();
+
+    for (int i = 0; i < SearchAgents; i++)
         Theseis[i].resize(D);
-    sampleFromProblem(SearchAgents,Theseis,fitness);
+
+    sampleFromProblem(SearchAgents, Theseis, fitness);
 
     upper = myProblem->getLeftMargin();
     lower = myProblem->getRightMargin();
     bestX.resize(D);
 
-    fitness_old.resize(SearchAgents, vector<double>(iters, numeric_limits<double>::max()));
-    position_old.resize(SearchAgents, vector<vector<double>>(iters, vector<double>(D, 0)));
-    troxies.resize(SearchAgents, vector<double>(iters, 0));
-    kambili.resize(iters, numeric_limits<double>::max());
+    fitness_old.resize(SearchAgents, vector<double>(maxGenerations, numeric_limits<double>::max()));
+    position_old.resize(SearchAgents, vector<vector<double>>(maxGenerations, vector<double>(D, 0)));
+
+    troxies.resize(SearchAgents, vector<double>(maxGenerations, 0));
+    kambili.resize(maxGenerations, numeric_limits<double>::max());
 }
 
 double EGO::evaluate( vector<double>& solution, double& grouperBestFitness) {
@@ -100,7 +103,6 @@ bool EGO::terminated() {
     for(unsigned long i=0;i<fitness.size();i++)
         if(fitness[i]<besty) besty = fitness[i];
     besty = grouperBestFitness;
-    //printf("EGO. iter = %d  besty = %lf maxGenerations = %d \n",generation,besty,maxGenerations);
     if(generation>=maxGenerations) return true;
     if(terminationMethod=="doublebox")
         return doubleBox.terminate(besty);
@@ -124,36 +126,31 @@ void EGO::step() {
     Selection();
 
     while (!terminated()) {
+        if (t >= maxGenerations) break;
 
-
-        // Σχέση Eq(5)
-        a = 2 - 2 * (t / iters);
-
-        // Σχέση Eq(9)
-        starvation_rate = 100 * (t / iters);
+        a = 2 - 2 * (t / (double)maxGenerations);
+        starvation_rate = 100 * (t / (double)maxGenerations);
 
         for (unsigned long i = 0; i < Theseis.size(); ++i) {
-
-            // Σχέσεις Eq(6), Eq(7), Eq(3),Eq(4).
-            r1 = (double) rand() / RAND_MAX;
-            r2 = (double) rand() / RAND_MAX;
+            r1 = (double)rand() / RAND_MAX;
+            r2 = (double)rand() / RAND_MAX;
             r3 = (a - 2) * r1 + 2;
             r4 = 100 * r2;
             C1 = 2 * a * r1 - a;
             C2 = 2 * r1;
             b = a * r2;
 
-            for (unsigned long  j = 0; j < Theseis[i].size(); ++j) {
-                double randLeader = rand() %  SearchAgents;
-                double D_X_rand = abs(C2 *  Theseis[i][j] -  Theseis[randLeader][j]);
-                Theseis[i][j] =  Theseis[randLeader][j] + C1 * D_X_rand;
+            for (unsigned long j = 0; j < Theseis[i].size(); ++j) {
+                double randLeader = rand() % SearchAgents;
+                double D_X_rand = abs(C2 * Theseis[i][j] - Theseis[randLeader][j]);
+                Theseis[i][j] = Theseis[randLeader][j] + C1 * D_X_rand;
             }
-            newFitness = evaluate(Theseis[i],grouperBestFitness);
+
+            newFitness = evaluate(Theseis[i], grouperBestFitness);
             if (newFitness < grouperBestFitness) {
                 grouperBestFitness = newFitness;
                 grouperBestThesi = Theseis[i];
             }
-
 
             if (r4 <= starvation_rate) {
                 eelThesi.resize(D);
@@ -169,29 +166,21 @@ void EGO::step() {
             }
 
             for (int j = 0; j < D; ++j) {
-                p = (double) rand() / RAND_MAX;
-                distance2eel = abs( Theseis[i][j] - C2 * eelThesi[j]);
+                p = (double)rand() / RAND_MAX;
+                distance2eel = abs(Theseis[i][j] - C2 * eelThesi[j]);
 
-                //  Χ1 σύμφωνα με τη σχέση Eq(10)
                 X1 = C1 * distance2eel * exp(b * r3) * sin(r3 * 2 * M_PI) + eelThesi[j];
                 distance2grouper = abs(C2 * grouperBestThesi[j] - Theseis[i][j]);
-
-                // Χ2 σύμφωνα με τη σχέση Eq(11)
                 X2 = grouperBestThesi[j] + C1 * distance2grouper;
 
                 if (p < 0.5) {
-
-                    // Σχέση Eq(11)
                     Theseis[i][j] = (0.8 * X1 + 0.2 * X2) / 2;
                 } else {
-
-                    // Σχέση Eq(12)
                     Theseis[i][j] = (0.2 * X1 + 0.8 * X2) / 2;
                 }
             }
 
             fitness[i] = myProblem->statFunmin(Theseis[i]);
-
 
             GradientDescent* local = new GradientDescent();
             local->setProblem(myProblem);
@@ -201,35 +190,34 @@ void EGO::step() {
             double CurrentFitness = fitness[i];
             ((GradientDescent*)local)->setPoint(Theseis[i], CurrentFitness);
             local->solve();
-            vector<double> X(D);
+            std::vector<double> X(D);
             ((GradientDescent*)local)->getPoint(X, CurrentFitness);
             delete local;
 
             bool feasible = Feasibility(X);
             if (feasible && CurrentFitness < fitness[i]) {
-
                 Theseis[i] = X;
                 fitness[i] = CurrentFitness;
             }
-
         }
 
-        for (unsigned long j = 0; j <Theseis.size(); ++j) {
+        for (unsigned long j = 0; j < Theseis.size(); ++j) {
             for (unsigned long k = 0; k < lower.size(); ++k) {
-                if ( Theseis[j][k] > upper[k]) {
+                if (Theseis[j][k] > upper[k]) {
                     Theseis[j][k] = upper[k];
-                } else if ( Theseis[j][k] < lower[k]) {
+                } else if (Theseis[j][k] < lower[k]) {
                     Theseis[j][k] = lower[k];
                 }
             }
 
-            fitness[j] = evaluate( Theseis[j],grouperBestFitness);
-            fitness_old[j][t] = fitness[j];
-            position_old[j][t] = Theseis[j];
+            fitness[j] = evaluate(Theseis[j], grouperBestFitness);
 
-            troxies[j][t] =  Theseis[j][0];
+            if (j < fitness_old.size() && t < fitness_old[j].size()) {
+                fitness_old[j][t] = fitness[j];
+                position_old[j][t] = Theseis[j];
+            }
 
-            fitness[j]=myProblem->statFunmin(Theseis[j]);
+            troxies[j][t] = Theseis[j][0];
 
             if (fitness[j] < grouperBestFitness) {
                 grouperBestThesi = Theseis[j];
@@ -237,17 +225,16 @@ void EGO::step() {
             }
         }
 
-        kambili[t] = grouperBestFitness;
+        if (t < kambili.size()) {
+            kambili[t] = grouperBestFitness;
+        }
 
         ++t;
     }
 
-
-
     if (terminated()) {
         done();
     }
-
 }
 
 EGO::~ EGO()
